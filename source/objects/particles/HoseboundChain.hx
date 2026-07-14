d57c556880af34144c5f1203d1e4f2e93e5d3ddf
Sufferneer <sufferneer@yahoo.com>
2026-07-14 23:05:26 +0800

    Edit opacity

--- source/objects/particles/HoseboundChain.hx
+++ source/objects/particles/HoseboundChain.hx
@@ -5,15 +5,10 @@ class HoseboundChain extends FlxSprite {
 		super(x, y);
 		loadGraphic(Paths.image('game/particles/hoseboundChain'));
 		this.color = Constants.PLAYER_COLORS[playerIndex];
+		this.alpha = 0.25;
 		this.offset.x += this.width / 2;
 		this.offset.y += this.height / 2;
-		var leAngle = FlxG.random.int(-180, 180);
-		var leVelocity = FlxG.random.int(90, 180);
-		this.velocity.set(
-			Math.sin(leAngle * Constants.TO_RADIANS) * leVelocity,
-			Math.cos(leAngle * Constants.TO_RADIANS) * leVelocity
-		);
-		this.angle = leAngle;
+		this.angle = FlxG.random.int(-180, 180);
 		this.angularVelocity = FlxG.random.int(-3, 3, [0]) * 360;
 		FlxTween.tween(this, {alpha: 0}, 0.5, {
 			startDelay: 0.5,
